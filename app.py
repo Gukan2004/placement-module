753cde3e32acd4e0a74ece6b265264e1639f2182
Janarthanan-Gnanamurthy <131849478+Janarthanan-Gnanamurthy@users.noreply.github.com>
2023-05-19 17:14:01 +0000

    added flask and a lot more

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, jsonify
+
+app = Flask(__name__)
+
+QUES=[{
+    'qno':1,
+    'q':'what is your name'
+},
+{
+    'qno':2,
+    'q':'why are you gay'
+},
+{
+    'qno':3,
+    'q':'what is your problem'
+},
+{
+    'qno':4,
+    'q':'pls leave me alone'
+},
+{
+    'qno':5,
+    'q':'you are a shit'
+}]
+
+@app.route('/')
+def hello():
+    return render_template('login.html',ques=QUES)
+
+@app.route('/api/question')
+def list_ques():
+    return jsonify(QUES)
